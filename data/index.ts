43fe1d7a8174c2ed3a1a538008a9637f6b8d1a48
unknown <richard.arshiawilson@gmail.com>
2025-02-13 08:01:04 +0000

    morning session

--- data/index.ts
+++ data/index.ts
@@ -8,7 +8,7 @@ export const navItems = [
 export const gridItems = [
     {
         id: 1,
-        title: "Committed to seamless client partnerships and transparent dialogue",
+        title: "I prioritize client collaboration, fostering open communication ",
         description: "",
         className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
         imgClassName: "w-full h-full",
@@ -18,7 +18,7 @@ export const gridItems = [
     },
     {
         id: 2,
-        title: "Passionate about technology and innovative development",
+        title: "I'm very flexible with time zone communications",
         description: "",
         className: "lg:col-span-2 md:col-span-3 md:row-span-2",
         imgClassName: "",
@@ -49,8 +49,8 @@ export const gridItems = [
 
     {
         id: 5,
-        title: "Developing a cutting-edge peer-to-peer lending platform",
-        description: "peerRamp",
+        title: "Currently building a JS Animation library",
+        description: "The Inside Scoop",
         className: "md:col-span-3 md:row-span-2",
         imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
         titleClassName: "justify-center md:justify-start lg:justify-center",
@@ -59,7 +59,7 @@ export const gridItems = [
     },
     {
         id: 6,
-        title: "Ready to embark on a project together?",
+        title: "Do you want to start a project together?",
         description: "",
         className: "lg:col-span-2 md:col-span-3 md:row-span-1",
         imgClassName: "",
@@ -72,68 +72,68 @@ export const gridItems = [
 export const projects = [
     {
         id: 1,
-        title: "peerRamp - a cutting-edge peer-to-peer lending platform",
-        des: "Explore the wonders of lending system with ease and ....",
+        title: "3D Solar System Planets to Explore",
+        des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
         img: "/p1.svg",
         iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-        link: "/https://peer-ramp.vercel.app/",
+        link: "https://github.com/adrianhajdin?tab=repositories",
     },
     {
         id: 2,
-        title: "trucast - Future Of Fair Elections",
-        des: 'Remember that feeling after casting your vote? That moment of "I hope it makes it"? Yeah, we were not fans either. That is why we are building something better.',
+        title: "Yoom - Video Conferencing App",
+        des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
         img: "/p2.svg",
         iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-        link: "/https://truecast.mowblox.com/",
+        link: "https://github.com/adrianhajdin/zoom-clone",
     },
     {
         id: 3,
-        title: "HebronEdge - Corporate portfolio website",
-        des: "Dedicated to empowering our clients' success by providing exceptional PR & media consultancy, event management, digital marketing, and IT services. We strive to be the catalyst that transforms our clients' aspirations into tangible, measurable results.",
-        img: "/hebronedge.svg",
+        title: "AI Image SaaS - Canva Application",
+        des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+        img: "/p3.svg",
         iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-        link: "/https://hebronedgeinc.vercel.app/",
+        link: "https://github.com/adrianhajdin/ai_saas_app",
     },
     {
         id: 4,
-        title: "VaultNotesAi",
-        des: "VaultNotesAi - ICP based project that helps users take memo, transcribe audio to text, and many more",
-        img: "/ailogo.svg",
+        title: "Animated Apple Iphone 3D Website",
+        des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+        img: "/p4.svg",
         iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-        link: "",
+        link: "https://github.com/adrianhajdin/iphone",
     },
 ];
 
 export const testimonials = [
     {
         quote:
-            "Working with Richard was a game-changer. His expertise, reliability, and innovative approach brought our project to life. His ability to translate complex requirements into elegant solutions is unmatched.",
-        name: "Sarah Thompson",
-        title: "CEO of Tanic Technologies",
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
     },
     {
         quote:
-            "He is an exceptional developer who always delivers top-notch results. His commitment to quality and user-centric design has greatly enhanced our digital presence. I highly recommend him for any development project.",
-        name: "John Kamu",
-        title: "CTO of Belp Inc.",
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
     },
     {
         quote:
-            "Partnering with Richard on our AI initiative was a transformative experience. His deep understanding of AI technologies and meticulous attention to detail ensured the success of our project. Richard is truly a master of his craft.",
-        name: "Alikamtu DevRel",
-        title: "Head of AI Research at Vault Labs",
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
     },
     {
         quote:
-            "This guy's talent for smart contract development is unparalleled. His strategic insights and technical prowess have been instrumental in launching our blockchain platform. He is a visionary developer with a bright future.",
-        name: "John Nartey",
-        title: "Co-founder of peerRamp",
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
     },
     {
         quote:
-            "His ability to blend creativity with technical skills has been a tremendous asset to our team. His work consistently exceeds expectations and elevates our projects. I look forward to collaborating with Richard again.",
-        name: "Jessica Dickson",
-        title: "UI/UX Lead at CodeGirls",
+            "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+        name: "Michael Johnson",
+        title: "Director of AlphaStream Technologies",
     },
 ];
 
@@ -173,14 +173,14 @@ export const companies = [
 export const workExperience = [
     {
         id: 1,
-        title: "Frontend Engineer",
-        desc: "Supervied the development of a web-based platform using React.js/Next.js enhancing interactivity.",
+        title: "Frontend Engineer Intern",
+        desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
         className: "md:col-span-2",
         thumbnail: "/exp1.svg",
     },
     {
         id: 2,
-        title: "Mobile App Dev - EduDesk Inc.",
+        title: "Mobile App Dev - JSM Tech",
         desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
         className: "md:col-span-2", // change to md:col-span-2
         thumbnail: "/exp2.svg",
@@ -194,8 +194,8 @@ export const workExperience = [
     },
     {
         id: 4,
-        title: "Lead IT Specialist",
-        desc: "Oversaw and maintained IT systems and security operations using modern and up-to-date technologies.",
+        title: "Lead Frontend Developer",
+        desc: "Developed and maintained user-facing features using modern frontend technologies.",
         className: "md:col-span-2",
         thumbnail: "/exp4.svg",
     },
@@ -205,44 +205,13 @@ export const socialMedia = [
     {
         id: 1,
         img: "/git.svg",
-        alt: "GitHub",
-        link: "https://github.com/dr-winner",
     },
     {
         id: 2,
         img: "/twit.svg",
-        alt: "X",
-        link: "https://x.com/dr_winner6",
     },
     {
         id: 3,
-        img: "/farcaster.svg",
-        alt: "Warpcast",
-        link: "https://warpcast.com/drwinner",
-    },
-    {
-        id: 4,
         img: "/link.svg",
-        alt: "LinkedIn",
-        link: "https://www.linkedin.com/in/richard-winner-duvor/",
-    },
-    {
-        id: 4,
-        img: "/insta.svg",
-        alt: "Instagram",
-        link: "https://www.instagram.com/winner.richard",
-    },
-    {
-        id: 5,
-        img: "/discord.svg",
-        alt: "Discord",
-        link: "https://discord.com/users/dr_winner",
-    },
-
-    {
-        id: 6,
-        img: "/behance.svg",
-        alt: "Behance",
-        link: "https://www.behance.net/duvorrichard",
     },
 ];
